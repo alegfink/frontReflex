c083c89d02459da9cdc495126befb3884b0de23b
alegfink <alegfink@gmail.com>
2024-05-08 19:58:26 -0300

    muchos cambios docker etc

---
+++ link_bio/public/remote_build.sh
@@ -0,0 +1,13 @@
+cd link_bio
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+API_URL=https://reflex-testing.up.railway.app reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
+
+# AVERIGUAR COMO SE USA
---
+++ link_bio/public/rxconfig.py
@@ -0,0 +1,7 @@
+import reflex as rx
+
+config = rx.Config(
+    app_name="link_bio",
+    # esta es una manera de agregar url de back para que conecte con el front
+    #api_url='https://reflex-testing.up.railway.app'  
+)
