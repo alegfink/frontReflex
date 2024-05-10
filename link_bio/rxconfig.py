6983ab67876b490b9cb20bca175ec328e54d2990
alegfink <alegfink@gmail.com>
2024-05-10 17:00:28 -0300

    github action

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -3,5 +3,10 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio",
     # esta es una manera de agregar url de back para que conecte con el front
-    #api_url='https://reflex-testing.up.railway.app'  
+    api_url='https://frontreflex-production.up.railway.app',  
+    # cors es para tener mas seguridad sobre las conexiones entre urls
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://frontreflex-production.up.railway.app"
+    ]
 )
